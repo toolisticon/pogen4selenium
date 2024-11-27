44466f39626fef32b8dc6c7203ded9138666622d
Tobias Stamann <tobias@stamann.dev>
2024-11-28 00:30:12 +0100

    Fixed issues with element lookup in runtime classes

--- pogen4selenium-api/src/main/java/io/toolisticon/pogen4selenium/runtime/PageObjectParentImpl.java
+++ pogen4selenium-api/src/main/java/io/toolisticon/pogen4selenium/runtime/PageObjectParentImpl.java
@@ -81,8 +81,7 @@ public abstract class PageObjectParentImpl <PAGEOBJECT extends PageObjectParent<
 		Wait<WebDriver> wait =
     	        new FluentWait<>(driver)
     	            .withTimeout(Duration.ofSeconds(25))
-    	            .pollingEvery(Duration.ofMillis(300))
-    	            .ignoring(ElementNotInteractableException.class);
+    	            .pollingEvery(Duration.ofMillis(300));
     	
     	wait.until(ExpectedConditions.urlMatches(urlRegex));
 	}
@@ -98,7 +97,7 @@ public abstract class PageObjectParentImpl <PAGEOBJECT extends PageObjectParent<
     	        new FluentWait<>(driver)
     	            .withTimeout(Duration.ofSeconds(15))
     	            .pollingEvery(Duration.ofMillis(300))
-    	            .ignoring(ElementNotInteractableException.class);
+    	            .ignoring(NoSuchElementException.class,ElementNotInteractableException.class);
     	
     	return wait.until(ExpectedConditions.elementToBeClickable(by));
 	}
